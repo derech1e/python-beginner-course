035ca17f6e267c1c3260f1ae199e3ea914b062cb
derech1e <nurkthomas@gmail.com>
2024-01-09 21:06:58 +0100

    update eight lesson

--- 008/c9_game_logic/finish/Direction.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum  # Import hier benötigt, um ein Enum zu verwenden
-
-
-class Direction(Enum):
-    STOP = "Stop"
-    LEFT = "Left"
-    RIGHT = "Right"
-    UP = "Up"
-    DOWN = "Down"
