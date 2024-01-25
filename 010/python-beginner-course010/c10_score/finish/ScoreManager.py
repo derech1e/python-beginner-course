8102e1a5abf13e716097c381673da7c799d0a3d5
derech1e <nurkthomas@gmail.com>
2024-01-25 21:52:55 +0100

    improve c10 and readme

--- 010/python-beginner-course010/c10_score/finish/ScoreManager.py
+++ 010/python-beginner-course010/c10_score/finish/ScoreManager.py
@@ -2,9 +2,6 @@ import turtle
 
 
 class ScoreManager:
-    high_score = 0
-    current_score = 0
-
     def __init__(self):
         self.pencil = turtle.Turtle()
         self.pencil.speed(0)
@@ -12,19 +9,22 @@ class ScoreManager:
         self.pencil.color('white')
         self.pencil.penup()
         self.pencil.hideturtle()
-        self.__draw_score(0)
+        self.high_score = 0
+        self.current_score = 0
+        self.__draw_score()
 
-    def __draw_score(self, current_score):
+    def __draw_score(self):
         self.pencil.clear()
         self.pencil.goto(0, 310)
-        self.pencil.write(f'Score: {current_score} High Score: {self.high_score}', align='center', font=('Courier', 24, 'normal'))
+        self.pencil.write(f'Score: {self.current_score} High Score: {self.high_score}', align='center',
+                          font=('Courier', 24, 'normal'))
 
     def add(self, score):
-        if self.current_score + score > self.high_score:
-            self.high_score += score
         self.current_score += score
-        self.__draw_score(self.current_score)
+        if self.current_score > self.high_score:
+            self.high_score = self.current_score
+        self.__draw_score()
 
     def reset(self):
         self.current_score = 0
-        self.__draw_score(0)
+        self.__draw_score()
